b33c3fa637685c6a06fbeca5f37a65e05e2ae889
Harold0411 <haroldaguadomaderazo@gmail.com>
2025-07-19 15:53:11 +0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1752929591931

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_bb1b4174._.js",
+  "static/chunks/node_modules_b7eea061._.js"
+],
+    source: "dynamic"
+});
